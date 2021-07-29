efc4b1a962425247cf6a205025af17cdc4f8eb96
apopesc <andrei_popescu@tjxcanada.ca>
2021-07-29 15:10:53 -0400

    Populating dropdowns with controller data

--- Controllers/SubscriptionGroupsController.cs
+++ Controllers/SubscriptionGroupsController.cs
@@ -12,16 +12,19 @@ namespace eComm_Reporting_Application.Controllers
         public IActionResult Index()
         {
             bool is_active = false;
+            
             List<string> groupsID_list = new List<string>();
             groupsID_list.Add("SVP");
             groupsID_list.Add("MM");
             groupsID_list.Add("Admin");
             groupsID_list.Add("NYBO MM Assist");
+            
             List<string> groups_list = new List<string>();
             groups_list.Add("Ecomm SVP, Merchandising and Planning");
             groups_list.Add("Ecomm Merchandise Managers");
             groups_list.Add("Ecomm Admins");
             groups_list.Add("Ecomm Merch Admins");
+            
             List<string> master_groupsID_list = new List<string>();
             master_groupsID_list.Add("Merchandising");
             master_groupsID_list.Add("Admin");
