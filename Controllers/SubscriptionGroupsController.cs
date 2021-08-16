eff6b5b0805243e1df6a31081dc7edf4406bc826
apopesc <andrei_popescu@tjxcanada.ca>
2021-08-16 10:41:35 -0400

    Pulling Data and Displaying it In the Filters on Page

--- Controllers/SubscriptionGroupsController.cs
+++ Controllers/SubscriptionGroupsController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using eComm_Reporting_Application.Models;
 using Microsoft.Extensions.Configuration;
 using System.Data.SqlClient;
+using System.Data;
 
 namespace eComm_Reporting_Application.Controllers
 {
@@ -19,39 +20,50 @@ namespace eComm_Reporting_Application.Controllers
 
         public IActionResult Index()
         {
+            int is_active = 0;
             //Testing pulling data from the DB -------------------------------------------------------------------------------//
             string connectionstring = configuration.GetConnectionString("ReportSubscriptions_DB");
 
             SqlConnection connection = new SqlConnection(connectionstring);
-            connection.Open();
-            SqlCommand com = new SqlCommand("Select count(*) from UserSubscriptionFilters", connection);
+            
+            SqlCommand groupsQuery = new SqlCommand("SELECT DISTINCT User_Group FROM UserSubscriptionFilters WHERE User_Group IS NOT NULL", connection);
+            SqlCommand groupIDsQuery = new SqlCommand("SELECT DISTINCT Group_ID FROM UserSubscriptionFilters WHERE Group_ID IS NOT NULL", connection);
+            SqlCommand masterGroupsQuery = new SqlCommand("SELECT DISTINCT Master_Group FROM UserSubscriptionFilters WHERE Master_Group IS NOT NULL", connection);
 
-            var count = (int)com.ExecuteScalar();
-            connection.Close();
-            //----------------------------------------------------------------------------------------------------------------//
-            
-            int is_active = 0;
-            
-            // ------------------- Temporarily Hard Coded Data, will be pulling this from DB in the future ------------------//
-            List<string> groupsID_list = new List<string>();
-            groupsID_list.Add("SVP");
-            groupsID_list.Add("MM");
-            groupsID_list.Add("Admin");
-            groupsID_list.Add("NYBO MM Assist");
-            
             List<string> groups_list = new List<string>();
-            groups_list.Add("Ecomm SVP, Merchandising and Planning");
-            groups_list.Add("Ecomm Merchandise Managers");
-            groups_list.Add("Ecomm Admins");
-            groups_list.Add("Ecomm Merch Admins");
-            
+            List<string> groupsID_list = new List<string>();
             List<string> master_groups_list = new List<string>();
-            master_groups_list.Add("Merchandising");
-            master_groups_list.Add("Admin");
-            master_groups_list.Add("Fulfillment");
-            master_groups_list.Add("Ecomm IT");
-            //--------------------------------------------------------------------------------------------------------------//
 
+            using (connection)
+            {
+                connection.Open();
+                using (SqlDataReader reader = groupsQuery.ExecuteReader())
+                {
+                    while(reader.Read())
+                    {
+                        var groupString = reader.GetString(0);
+                        groups_list.Add(groupString);
+                    }
+                }
+                using (SqlDataReader reader = groupIDsQuery.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var groupIDString = reader.GetString(0);
+                        groupsID_list.Add(groupIDString);
+                    }
+                }
+                using (SqlDataReader reader = masterGroupsQuery.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var masterGroupString = reader.GetString(0);
+                        master_groups_list.Add(masterGroupString);
+                    }
+                }
+                connection.Close();
+            }
+            
             SubscriptionGroupsModel subModel = new SubscriptionGroupsModel() {
                 isActive = is_active,
                 groupsIDList = groupsID_list,
