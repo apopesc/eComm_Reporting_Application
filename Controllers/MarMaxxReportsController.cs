95bd2a7bd5c63507d54ab3a3ba60a9ae7518deef
apopesc <andrei_popescu@tjxcanada.ca>
2021-08-31 14:08:52 -0400

    Calling controller function on dropdown change

--- Controllers/MarMaxxReportsController.cs
+++ Controllers/MarMaxxReportsController.cs
@@ -22,69 +22,21 @@ namespace eComm_Reporting_Application.Controllers
             marMaxxDropdownModel.folderList = folder_list;
             return View(marMaxxDropdownModel);
         }
+
+        [HttpPost]
+        public JsonResult GetReportNameValues(List<string> folderList)
+        {
+            try
+            {
+                return Json(folderList);
+            }
+            catch (Exception e)
+            {
+                return Json("Error retrieving table data: " + e);
+            }
+
+        }
     }
 
-    [HttpPost]
-    public JsonResult GetTableData(UserSubscriptionDropdownModel filterData)
-    {
-        try
-        {
-            tableData = new List<UserSubscriptionTableModel>();//Resetting the table each time we want to get new data
-
-            string connectionstring = configuration.GetConnectionString("ReportSubscriptions_DB");
-            SqlConnection connection = new SqlConnection(connectionstring);
-            SqlCommand tableQuery;
-
-            //Setting the isActive value for the query
-            string isActiveString;
-            if (filterData.isActive == 1)
-            {
-                isActiveString = "'Y'";
-            }
-            else if (filterData.isActive == 2)
-            {
-                isActiveString = "'N'";
-            }
-            else //If both are selected, show all data for isActive
-            {
-                isActiveString = "'Y', 'N'";
-            }
-
-            //Converting lists to strings for the query
-            string groupsListString = String.Join("', '", filterData.groupsList.ToArray());
-            groupsListString = "'" + groupsListString + "'";
-            string masterGroupsListString = String.Join("', '", filterData.masterGroupsList.ToArray());
-            masterGroupsListString = "'" + masterGroupsListString + "'";
-
-            tableQuery = new SqlCommand("SELECT * FROM UserSubscriptions WHERE Is_Active IN (" + isActiveString + ") AND User_Group IN (" + groupsListString + ") AND Master_Group IN (" + masterGroupsListString + ");", connection);
-
-            using (connection)
-            {
-                connection.Open();
-                using (SqlDataReader reader = tableQuery.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        UserSubscriptionTableModel entry = new UserSubscriptionTableModel();
-                        entry.ID = reader.GetInt32(0);
-                        entry.userEmail = reader.GetString(1);
-                        entry.isActive = reader.GetString(2);
-                        entry.group = reader.GetString(3);
-                        entry.groupID = reader.GetString(4);
-                        entry.masterGroup = reader.GetString(5);
-                        tableData.Add(entry);
-                    }
-                }
-                connection.Close();
-            }
-
-            //Returning the table data to the front end
-            return Json(tableData);
-        }
-        catch (Exception e)
-        {
-            return Json("Error retrieving table data: " + e);
-        }
-
-    }
+    
 }
--- wwwroot/js/marMaxxReports.js
+++ wwwroot/js/marMaxxReports.js
@@ -19,6 +19,29 @@ function selectedFolder() {
     if ($('#marMaxxFolderDropdown :selected').length == 0) { //Nothing is selected in the dropdown (last value is deselected)
         $('#marMaxxReportDropdown').multiselect('disable');
     } else { //Something is selected in the dropdown
-        $('#marMaxxReportDropdown').multiselect('enable');
+        var controllerUrl = '/MarMaxxReports/GetReportNameValues';
+
+        var selectedFolders = $('#marMaxxFolderDropdown').find(":selected").text();
+        var folderList = selectedFolders.split('  ');
+        for (let i = 0; i < folderList.length; i++) {
+            folderList[i] = folderList[i].trim();
+        }
+
+        $.ajax({
+            type: "POST",
+            url: controllerUrl,
+            dataType: "json",
+            success: successFunc,
+            error: errorFunc,
+            data: { 'folderList': folderList }
+        });
+
+        function successFunc(dropdownData) {
+            $('#marMaxxReportDropdown').multiselect('enable');
+        }
+
+        function errorFunc(error) {
+            alert("Error Retrieving Report Names: " + error);
+        }
     }
 }
