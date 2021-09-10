17f4b3ca5e798184dccfd559fd6a5d6d1cb597d9
apopesc <andrei_popescu@tjxcanada.ca>
2021-09-10 11:43:35 -0400

    Built Out MarMaxx Subscriptions Database
    Moved get report parameters to a function as well

--- Controllers/MarMaxxReportsController.cs
+++ Controllers/MarMaxxReportsController.cs
@@ -84,76 +84,17 @@ namespace eComm_Reporting_Application.Controllers
         {
             try
             {
-                List<ReportParameterModel> reportParameters = new List<ReportParameterModel>();
+                ReportParameterModel tableParameters = getReportParameters(reportName);
 
-                string connectionstring = configuration.GetConnectionString("ReportServer");
-                SqlConnection connection = new SqlConnection(connectionstring);
-
-                string queryString = "DROP TABLE IF EXISTS #Parameters DROP TABLE IF EXISTS #TempReportParameterTable " +
-                    "SELECT Folder,Report_Name,Full_Path,Parameter = Paravalue.value('Name[1]', 'VARCHAR(250)'),Type = Paravalue.value('Type[1]', 'VARCHAR(250)')," +
-                    "Nullable = Paravalue.value('Nullable[1]', 'VARCHAR(250)'),AllowBlank = Paravalue.value('AllowBlank[1]', 'VARCHAR(250)'),MultiValue = Paravalue.value('MultiValue[1]', 'VARCHAR(250)')," +
-                    "UsedInQuery = Paravalue.value('UsedInQuery[1]', 'VARCHAR(250)'),Prompt = Paravalue.value('Prompt[1]', 'VARCHAR(250)'),DynamicPrompt = Paravalue.value('DynamicPrompt[1]', 'VARCHAR(250)')," +
-                    "PromptUser = Paravalue.value('PromptUser[1]', 'VARCHAR(250)'),State = Paravalue.value('State[1]', 'VARCHAR(250)') " +
-                    "INTO #Parameters " +
-                    "FROM (SELECT LEFT(Path, Len(Path)-Len(Name)-1) AS Folder,Name AS Report_Name,Path as Full_Path,CONVERT(XML,C.Parameter) AS ParameterXML FROM ReportServer.dbo.Catalog C WHERE C.Content IS NOT NULL AND C.Type = 2) a " +
-                    "CROSS APPLY ParameterXML.nodes('//Parameters/Parameter') p ( Paravalue ) " +
-                    "SELECT LEFT(Path, Len(Path)-Len(Name)-1) AS Folder,Name,Path as FullPath," +
-                    "STUFF((SELECT ', '+ISNULL(Parameter,'') FROM #Parameters P WHERE C.Name = P.Report_Name AND PromptUser = 'True' AND Parameter NOT LIKE '%hidden%' AND Parameter NOT IN ('NoMonths','Job_Type') FOR XML PATH ('')),1, 1, '') AS [Parameters]," +
-                    "STUFF((SELECT ', '+ISNULL(Parameter,'') FROM #Parameters P WHERE C.Name = P.Report_Name FOR XML PATH ('')),1, 1, '') AS Parameters_WithHidden " +
-                    "INTO #TempReportParameterTable " +
-                    "FROM ReportServer.dbo.Catalog C WITH(NOLOCK) WHERE TYPE = 2 AND Left(Path, Len(Path)-Len(Name)-1) NOT IN ('/DevTest Reports','/PreProdTestReports') ORDER BY 1,2 " +
-                    "SELECT * FROM #TempReportParameterTable WHERE Name IN ('" + reportName + "');";
-
-                SqlCommand getFolderList = new SqlCommand(queryString, connection);
-                using (connection)
+                if(tableParameters.parameters == null)
                 {
-                    connection.Open();
-                    using (SqlDataReader reader = getFolderList.ExecuteReader())
-                    {
-                        while (reader.Read())
-                        {
-                            ReportParameterModel reportParams = new ReportParameterModel();
-
-                            //There is other data returned by this query that can be used - such as folder, fullpath, and hidden parameters.
-                            //Currently only using report name and parameters
-                            var report_name = reader.GetString(1);
-                            if (reader.IsDBNull(3))
-                            {
-                                return Json(report_name + " has no parameters, please select a report with parameters");
-                            }
-                            else
-                            {
-                                var report_params_string = reader.GetString(3);
-                                List<string> report_params_list = new List<string>(report_params_string.Split(", "));
-                                reportParams.reportName = report_name;
-                                reportParams.parameters = report_params_list;
-
-                                reportParameters.Add(reportParams);
-                            }
-                        }
-                    }
-                    connection.Close();
+                    return Json(reportName + " has no parameters, please select a report with parameters");
                 }
-
-                //Filtering duplicate parameter data
-                ReportParameterModel tableParameters = new ReportParameterModel();
-                List<string> parameters = new List<string>();
-
-                foreach(ReportParameterModel paramModel in reportParameters)
+                else
                 {
-                    foreach(string parameter in paramModel.parameters)
-                    {
-                        string trimmed_param = parameter.Trim(' ');
-                        if (!parameters.Contains(trimmed_param))
-                        {
-                            parameters.Add(trimmed_param);
-                        }
-                    }
+                    return Json(tableParameters);
                 }
-                tableParameters.reportName = reportParameters[0].reportName;
-                tableParameters.parameters = parameters;
-
-                return Json(tableParameters);
+                
             }
             catch (Exception e)
             {
@@ -198,5 +139,83 @@ namespace eComm_Reporting_Application.Controllers
             return dropdownModel;
         }
 
+        public ReportParameterModel getReportParameters(string reportName)
+        {
+            List<ReportParameterModel> reportParameterModelList = new List<ReportParameterModel>();
+            ReportParameterModel filteredReportParameters = new ReportParameterModel();
+            try
+            {
+                string connectionstring = configuration.GetConnectionString("ReportServer");
+                SqlConnection connection = new SqlConnection(connectionstring);
+
+                string queryString = "DROP TABLE IF EXISTS #Parameters DROP TABLE IF EXISTS #TempReportParameterTable " +
+                    "SELECT Folder,Report_Name,Full_Path,Parameter = Paravalue.value('Name[1]', 'VARCHAR(250)'),Type = Paravalue.value('Type[1]', 'VARCHAR(250)')," +
+                    "Nullable = Paravalue.value('Nullable[1]', 'VARCHAR(250)'),AllowBlank = Paravalue.value('AllowBlank[1]', 'VARCHAR(250)'),MultiValue = Paravalue.value('MultiValue[1]', 'VARCHAR(250)')," +
+                    "UsedInQuery = Paravalue.value('UsedInQuery[1]', 'VARCHAR(250)'),Prompt = Paravalue.value('Prompt[1]', 'VARCHAR(250)'),DynamicPrompt = Paravalue.value('DynamicPrompt[1]', 'VARCHAR(250)')," +
+                    "PromptUser = Paravalue.value('PromptUser[1]', 'VARCHAR(250)'),State = Paravalue.value('State[1]', 'VARCHAR(250)') " +
+                    "INTO #Parameters " +
+                    "FROM (SELECT LEFT(Path, Len(Path)-Len(Name)-1) AS Folder,Name AS Report_Name,Path as Full_Path,CONVERT(XML,C.Parameter) AS ParameterXML FROM ReportServer.dbo.Catalog C WHERE C.Content IS NOT NULL AND C.Type = 2) a " +
+                    "CROSS APPLY ParameterXML.nodes('//Parameters/Parameter') p ( Paravalue ) " +
+                    "SELECT LEFT(Path, Len(Path)-Len(Name)-1) AS Folder,Name,Path as FullPath," +
+                    "STUFF((SELECT ', '+ISNULL(Parameter,'') FROM #Parameters P WHERE C.Name = P.Report_Name AND PromptUser = 'True' AND Parameter NOT LIKE '%hidden%' AND Parameter NOT IN ('NoMonths','Job_Type') FOR XML PATH ('')),1, 1, '') AS [Parameters]," +
+                    "STUFF((SELECT ', '+ISNULL(Parameter,'') FROM #Parameters P WHERE C.Name = P.Report_Name FOR XML PATH ('')),1, 1, '') AS Parameters_WithHidden " +
+                    "INTO #TempReportParameterTable " +
+                    "FROM ReportServer.dbo.Catalog C WITH(NOLOCK) WHERE TYPE = 2 AND Left(Path, Len(Path)-Len(Name)-1) NOT IN ('/DevTest Reports','/PreProdTestReports') ORDER BY 1,2 " +
+                    "SELECT * FROM #TempReportParameterTable WHERE Name IN ('" + reportName + "');";
+
+                SqlCommand getReportParams = new SqlCommand(queryString, connection);
+                using (connection)
+                {
+                    connection.Open();
+                    using (SqlDataReader reader = getReportParams.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            ReportParameterModel reportParams = new ReportParameterModel();
+
+                            //There is other data returned by this query that can be used - such as folder, fullpath, and hidden parameters.
+                            //Currently only using report name and parameters
+                            var report_name = reader.GetString(1);
+                            if (reader.IsDBNull(3))
+                            {
+                                return filteredReportParameters; //Returning empty list of parameters if the report has no parameters
+                            }
+                            else
+                            {
+                                var report_params_string = reader.GetString(3);
+                                List<string> report_params_list = new List<string>(report_params_string.Split(", "));
+                                reportParams.reportName = report_name;
+                                reportParams.parameters = report_params_list;
+
+                                reportParameterModelList.Add(reportParams);
+                            }
+                        }
+                    }
+                    connection.Close();
+                }
+
+                //Filtering duplicate parameter data
+                List<string> parameters = new List<string>();
+
+                foreach (ReportParameterModel paramModel in reportParameterModelList)
+                {
+                    foreach (string parameter in paramModel.parameters)
+                    {
+                        string trimmed_param = parameter.Trim(' ');
+                        if (!parameters.Contains(trimmed_param))
+                        {
+                            parameters.Add(trimmed_param);
+                        }
+                    }
+                }
+                filteredReportParameters.reportName = reportParameterModelList[0].reportName;
+                filteredReportParameters.parameters = parameters;
+            }
+            catch (Exception e)
+            {
+                //Redirect to error page and pass forward exception e once error page is set up.
+            }
+            return filteredReportParameters;
+        }
     }  
 }
